55b9f2bbc1a73242c88be8f39467f4173ca7e07b
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-02-10 19:27:02 +0530

    Part-1
    Part-1(MERN)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# eshopmern
+# e-shop
+this is  my first mern project 
+mongodb+srv://avinashgupta380:<password>@cluster0.bilyfsp.mongodb.net/
+
+
+pmt
+mongodb+srv://avianshgupta:eshop123@atlascluster.xtp3gat.mongodb.net/
---
+++ frontend/src/Routes.js
@@ -0,0 +1,12 @@
+import LoginPage from "../src/pages/Login";
+import SignupPage from "../src/pages/SignupPage";
+import ActivationPage from "../src/pages/ActivationPage";
+import HomePage from "../src/pages/HomePage";
+import ProductsPage from "../src/pages/ProductsPage";
+import BestSellingPage from "../src/pages/BestSellingPage.jsx";
+import EventsPage from "../src/pages/EventsPage.jsx";
+import FAQPage from "../src/pages/FAQPage.jsx";
+
+export {
+    LoginPage,SignupPage,ActivationPage,HomePage,ProductsPage,BestSellingPage,EventsPage,FAQPage
+};
